bb83ef041eaeaf87db01ba4ef26a152b91155342
goureeshreddy <goureeshreddy7@gmail.com>
2026-03-24 01:32:09 +0530

    goo

---
+++ CF/B. String LCM.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    int q;con>>q;
+    while(t--){
+        string s,t;
+        cin>>s>>t;
+        int a=0;
+        if(s.length()>t.length()){
+            int k=t.lengthI();
+            for(int i=0;i<k;i++){
+                
+            }
+        }
+    }
+}   
