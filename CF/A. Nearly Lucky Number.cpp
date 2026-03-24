d80837ac91d6c66e2f46d350f5ce896ab4372c64
goureeshreddy <goureeshreddy7@gmail.com>
2026-03-25 00:26:53 +0530

    goo

--- CF/A. Nearly Lucky Number.cpp
+++ CF/A. Nearly Lucky Number.cpp
@@ -8,18 +8,21 @@ int main() {
     long long n;
     cin>>n;
     vector<int>v;
-    int k=n;
-    while(k){
-        int p=k%10;
-        k/=10;
+    //long long k=n;
+    //before i used int for k so not taking value at all
+    // and now i am good with long long 
+    // and i dont know why i am persuing value of n litterally :-)
+    while(n){
+        int p=n%10;
+        n/=10;
         if(p==4||p==7){
             v.push_back(p);
         }
     }
     if(v.size()==4||v.size()==7){
-        cout<<"YES"<<v.size();
+        cout<<"YES";
 
     }else{
-        cout<<"NO "<<v.size();
+        cout<<"NO ";
     }
 }   
